8e3c66668dcc0871b7d2ef675887159d21f4f6f0
Francisco Seguí <francisco.segui.or@gmail.com>
2021-06-28 00:50:42 -0300

    done

--- ejerciciofinal-FranciscoSegui.py
+++ ejerciciofinal-FranciscoSegui.py
@@ -1,15 +1,12 @@
+# In[1]
 """
 ==================
 Final Assignment
 ==================
 
+Alumno: Francisco Seguí
 
 """
-#print(__doc__)
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
 print('ECD')
 print('Este ejercicio tiene dos maneras de resolverse.')
 print('Lo tiene que tener listo para el fin de la Cuarentena')
@@ -17,9 +14,7 @@ print('Lo tiene que tener listo para el fin de la Cuarentena')
 print('Opción B: elijan una (al menos) pregunta e intentan implementar una solución, codificando en R, Java o python.')
 
 print('0 - Construyan una alternativa para detectar pestañeos (blinking.dat) y trabajen sobre el dataset de pestañeos para simular y testear el abordaje propuesto.')
-print('1 - De las señales del EPOC Emotiv que obtuvimos de SUJETO, intenten estudiar las señales detectando: los pestañeos sobre F8 y F7, el momento donde el sujeto cierra los ojos, donde abre y cierra la boca, donde mueve la cabeza haciendo Roll, y donde mueve la cabeza haciendo YAW.')
-print('2 - Sobre los datos de MNIST, intenten luego de clusterizar armar un clasificador.')
-print('3 - Busquen un dataset de internet público de señales de sensores.  ¿Cómo lo abordarían exploratoriamente, qué procesamiento y qué análisis harían?')
-print('4 - Prueben alternativas para mejorar la clasificación de las ondas alfa.')
-print('5 - ¿Que feature utilizarian para mejorar la clasificacion que ofrece Keras con MLP para las series de tiempo?')
-print('6 - Suban un snippet que aborde alguna problemática de solución, implementé algún otro método de clasificación o de análisis, sobre los datos registrados en este repositorio.')
+
+
+# In[2]
+
