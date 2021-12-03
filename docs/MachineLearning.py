bc12aa209b23ec1b39979d524938a46f80d639e7
Rodrigo Ramele <rramele@gmail.com>
2021-12-03 12:59:28 -0300

    Update MachineLearning.py

--- docs/MachineLearning.py
+++ docs/MachineLearning.py
@@ -1,1 +1,55 @@
 # Books
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Machine Learning a Probabilistic Perspective, Murphy</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>The Nature of Statistical Learning, Vapnik</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Elements of Information Theory, Cover</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>An Introduction to Machine Learning, Kubat</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Pattern Recognition and Machine Learning, Bishop</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Information Theory, Inference and Learning Algorithms, MacKay</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Machine Learning, Mello</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>The Elements of Statistical Learning, Hastie</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Understanding Machine Learning: From Theory to Algorithms, Shai Shalev-Schwartz</p></td></tr>
+</table>
+
+# Deep Learning
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Neural Networks and Deep Learning, Aggarwal</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Principles of Deep Learning Theory, Roberts and Yaida</p></td></tr>
+</table>
+
+# Reinforcement Learning
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Reinforcement Learning, Sutton</p></td></tr>
+</table>
+
+# Machine Learning and Control Theory
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Data-Driven Science and Engineering, Brunton</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Kalman Filtering and Neural Networks, Haykin</p></td></tr>
+</table>
+
+# Data Mining
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Data Mining and Analysis, Zaki</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Cluster Analysis, Everit</p></td></tr>
+</table>
+
+# Mathematics
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Linear Algebra and Optimization for Machine Learning, Aggarwal</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Algebra, Topology, Differencial Calculus, and Optimization Theory for Computer Science and Machine Learning, Gallier</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Introduction to Applied Linear Algebra, Stephen Boyd</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Mathematics for Machine Learning, Garret Thomas</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Mathematics for Machine Learning, Deisenroth</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Algorithms for Convex Optimization, Vishnoi</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Course notes on Optimization for Machine Learning, Gabriel Peyré</p></td></tr>
+</table>
+
+# Statistics
+<table>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Nonparametric Statistical Methods, Hollander</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Applied Regression Analysis, Draper</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Mathematics for Machine Learning, Garret Thomas</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Mathematics for Machine Learning, Deisenroth</p></td></tr>
+<tr><td><img src="primer.png" alt="Primer" style="height: 104px; width:78px;"/></td><td><p>Algorithms for Convex Optimization, Vishnoi</p></td></tr>
+</table>
