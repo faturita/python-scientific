6b8779042cd56efc52d186be80b0361ca8a4c0e2
faturita <rramele@gmail.com>
2022-06-23 00:34:56 +0100

    Subo el enunciado del ejercicio final, con la referencia a la ubicación del dataset.

--- ejerciciofinal.py
+++ ejerciciofinal.py
@@ -3,12 +3,34 @@
 Final Assignment
 ==================
 
+Entre 10 y 11 minutos
+Fs = 512
+
+
+|---- BASELINE --------|
+|---- TOSER ------|
+|---- RESPIRAR FONDO ------- |
+|---- RESPIRAR RAPIDO ----|
+|---- CUENTA MENTAL --------|
+|---- COLORES VIOLETA ------|
+|---- COLORES ROJO --------|
+|---- SONREIR -----|
+|---- DESEGRADABLE -----| 
+|---- AGRADABLE --------|
+|---- PESTANEOS CODIGO ------ |
+
+
+Los datos, el registro de EEG y el video, están disponibles en el siguiente link:
+https://drive.google.com/file/d/1ByQDK4ZPxbqw7T17k--avTcgSCCzs3vi/view?usp=sharing
 
 """
+
 print(__doc__)
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
+from io import StringIO
 
 # El protocolo experimental que implementamos tiene 2 datasets:
 # 1- Dataset de las señales de EEG
@@ -18,4 +40,22 @@ import matplotlib.pyplot as plt
 # La idea es tomar estos datasets y derivar de forma automática las diferentes secciones.  Esto se puede hacer en base self-supervised, es
 # decir tomar los datos de algún dataset, derivar los labels para cada secciones y luego intentar implementar un clasificador multiclase.
 #
-# 
+# Tienen que entregar un PDF, tipo Markdown con código, gráficos y cualquier insight obtenido del dataset.
+
+signals = pd.read_csv('data/B001/eeg.dat', delimiter=' ', names = ['timestamp','counter','eeg','attention','meditation','blinking'])
+
+print('Estructura de la informacion:')
+print(signals.head())
+
+data = signals.values
+eeg = data[:,2]
+
+print(eeg)
+
+plt.plot(eeg,'r', label='EEG')
+plt.xlabel('t');
+plt.ylabel('eeg(t)');
+plt.title(r'EEG Signal')     # r'' representa un raw string que no tiene caracteres especiales
+plt.ylim([-2000, 2000]);
+plt.xlim([0,len(eeg)])
+plt.show()
