3309b7f623a5b2456adc7d45255650707387ef80
faturita <rramele@gmail.com>
2024-11-14 22:57:36 -0300

    Fix the taxilearning example for qlearning.

--- taxilearning.py
+++ taxilearning.py
@@ -6,13 +6,23 @@ Q-Learning toy sample based on OpenAI Gym.
 Source: https://www.learndatasci.com/tutorials/reinforcement-q-learning-scratch-python-openai-gym/
         Sutton Book, "Reinforcement Learning"
 
-In RL, QLearning, we want to learn a Q table, a function, for which given a state we can check the all the different actions to determine which one has
-the greater value on the Q table, and pick that one.  It is very easy to implement, works very well, but it requires a lot of episodes.
+In RL, QLearning, we want to learn a Q table, a function, for which given a 
+state we can check the all the different actions to determine which one has
+the greater value on the Q table, and pick that one.  It is very easy to 
+implement, works very well, but it requires a lot of episodes.
 This Q table can be replaced by a neural network (as function approximator).
 
 5x5 grid
 4 destinations
 4+1 passengers locations (the 4 destinations plus inside the taxi)
+
++---------+
+|R: | : :G|
+| : | : : |
+| : : : : |
+| | : | : |
+|Y| : |B: |
++---------+
 
 6 possible actions
 
@@ -28,6 +38,9 @@ This Q table can be replaced by a neural network (as function approximator).
 """
 
 import gym
+import numpy as np
+import random
+import matplotlib.pyplot as plt
 
 env = gym.make('Taxi-v3', render_mode="ansi").env
 
@@ -57,7 +70,10 @@ done = False
 
 while not done:
     action = env.action_space.sample()
+    #print(f"Action: {action}")
     state, reward, done, info,ext = env.step(action)
+    #print(f"State: {state}")
+
 
     if reward == -10:
         penalties += 1
@@ -78,6 +94,7 @@ print("Timesteps taken: {}".format(epochs))
 print("Penalties incurred: {}".format(penalties))
 
 
+
 # Let's see what is happening..... this is 100% exploration without any learning.
 #from IPython.display import clear_output
 from time import sleep, time
@@ -93,71 +110,101 @@ def print_frames(frames):
         sleep(.1)
         
 print_frames(frames)
+quit()
 
 
 # QLEarning rule, Q(s,a) <- (1-𝛂) Q(s,a) + 𝛂 ( R + γ max_an Q(next state, an)
 # Alpha is the explotation-exploration tradeoff whle gamma is the greedy-longterm term (0 is greedy)
-
-import numpy as np
-q_table = np.zeros([env.observation_space.n, env.action_space.n])
-
-import random
+env.reset()
 
 # Hyperparameters
 alpha = 0.1
 gamma = 0.6
 epsilon = 0.1
 
-# For plotting metrics
-all_epochs = []
-all_penalties = []
+q_table = np.zeros([5 * 5 * 5 * 4 , env.action_space.n])
 
-for i in range(1, 100001):
-    state = env.reset()
+reward_list = []
+ave_reward_list = []
 
-    epochs, penalties, reward, = 0, 0, 0
+tot_rewards = 0
+
+for i in range(1, 100000):
+    env.reset()
     done = False
-    
+    state=0
     while not done:
         if random.uniform(0, 1) < epsilon:
             action = env.action_space.sample() # Explore action space
         else:
-            action = np.argmax(q_table[state]) # Exploit learned values
+            action = np.argmax(q_table[state,:]) # Exploit learned values
 
-        next_state, reward, done, info, ext = env.step(action) 
-        
+        #print(f"Action: {action}")
+        next_state, reward, done, info,ext = env.step(action)
+        #print(f"State: {state} -> {next_state}")
+
         old_value = q_table[state, action]
-        next_max = np.max(q_table[next_state])
+        next_max = np.max(q_table[next_state,:])
         
         new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
         q_table[state, action] = new_value
 
+        tot_rewards += reward
         if reward == -10:
             penalties += 1
-
-        state = next_state
-        epochs += 1
         
-    if i % 100 == 0:
-        #clear_output(wait=True)
-        print(f"Episode: {i}")
+        # Put each rendered frame into dict for animation
+        frames.append({
+            'frame': env.render(),
+            'state': state,
+            'action': action,
+            'reward': reward
+            }
+        )
 
+        epochs += 1
+        state=next_state
+
+    # Track rewards
+    reward_list.append(tot_rewards)
+    penalties=0
+    tot_rewards=0
+    
+    if (i+1) % 100 == 0:
+        ave_reward = np.mean(reward_list)
+        ave_reward_list.append(ave_reward)
+        reward_list = []
+        
+    if (i+1) % 100 == 0:    
+        print('Episode {} Average Reward: {}'.format(i+1, ave_reward))
+
+plt.plot(100*(np.arange(len(ave_reward_list)) + 1), ave_reward_list)
+plt.xlabel('Episodes')
+plt.ylabel('Average Reward')
+plt.title('Average Reward vs Episodes')
+#plt.savefig('rewards.pdf')     
+#plt.close()  
+plt.show()
 print("Training finished.\n")
+
+
+
 
 """Evaluate agent's performance after Q-learning"""
 
 total_epochs, total_penalties = 0, 0
 episodes = 100
+total_penalties_optimal_list = []
 
 for _ in range(episodes):
-    state = env.reset()
+    env.reset()
     epochs, penalties, reward = 0, 0, 0
     
     done = False
-    
+    state=0
     while not done:
-        action = np.argmax(q_table[state])
-        state, reward, done, info = env.step(action)
+        action = env.action_space.sample()
+        state, reward, done, info, ext = env.step(action)
 
         if reward == -10:
             penalties += 1
@@ -166,7 +213,49 @@ for _ in range(episodes):
 
     total_penalties += penalties
     total_epochs += epochs
+    total_penalties_optimal_list.append(penalties)
 
+print(f"Random Actions")
+print(f"--------------")
 print(f"Results after {episodes} episodes:")
 print(f"Average timesteps per episode: {total_epochs / episodes}")
 print(f"Average penalties per episode: {total_penalties / episodes}")
+
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+total_penalties_random_list = []
+
+for _ in range(episodes):
+    env.reset()
+    epochs, penalties, reward = 0, 0, 0
+    
+    done = False
+    state=0
+    while not done:
+        action = np.argmax(q_table[state,:])
+        state, reward, done, info, ext = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        epochs += 1
+
+    total_penalties += penalties
+    total_epochs += epochs
+    total_penalties_random_list.append(penalties)
+
+print(f"Optimal Policy")
+print(f"--------------")
+print(f"Results after {episodes} episodes:")
+print(f"Average timesteps per episode: {total_epochs / episodes}")
+print(f"Average penalties per episode: {total_penalties / episodes}")
+
+
+
+plt.plot(total_penalties_random_list, label='Random Actions')
+plt.plot(total_penalties_optimal_list, label='Optimal Policy')
+plt.xlabel('Episodes')
+plt.ylabel('Penalties')
+plt.title('Penalties vs Episodes')
+plt.show()
